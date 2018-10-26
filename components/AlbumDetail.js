a01823962b738e563604e4b89ad877487cdeb281
Andy Sekulovski <andy.sek94@gmail.com>
2018-10-26 14:39:27 +1100

    added button and linking api to app

--- components/AlbumDetail.js
+++ components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({album}) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { headerContent, 
           thumbnailStyle, 
           thumbnailContainerStyle,
@@ -27,6 +28,9 @@ const AlbumDetail = ({album}) => {
       </CardSection>
       <CardSection>
         <Image style={imageStyle} source={{uri: image}}/>
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)} text={"Buy " + title}/>
       </CardSection>
     </Card>
   )
